13ee7ab4cdc530e9b84c15dc6dc5cd9e0d544bcb
sumit161459 <59919501+sumit161459@users.noreply.github.com>
2023-04-25 16:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from transformers import pipeline
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
+summarizer = pipeline("summarization")
+
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def hellp_world():
+    return 'Hello, World!'
+
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    youtube_video = data['text']
+    video_id = youtube_video.split("=")[1]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    corpus = " "
+    for i in transcript:
+        corpus += ' '+i['text']
+    # max_length = data.get('max_length', 100)
+    summary = summarizer(corpus)[0]['summary_text']
+    return jsonify({'summary': summary})
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(host="0.0.0.0", port="5000", debug=True)
